e675a6dc36f70715002d6e3ae33ab6bb467e92a0
Uthpala Hewage <uthpalahewage11@gmail.com>
2019-11-19 13:19:06 +0530

    adding word pattern for word filteration
    declare dict for word patterns
    used those patterns in word filteration(stop words declared)

---
+++ Model/rule_based_word_patterns.py
@@ -0,0 +1,7 @@
+dict = {}
+
+hello_p1 = [{'LOWER': 'hello'}, {'IS_PUNCT': True, 'OP': '?'}]
+however_p1 = [{'LOWER': 'however'}, {'IS_PUNCT': True, 'OP': '?'}]
+
+dict.update({"hello_p1": hello_p1})
+dict.update({"however_p1": however_p1})
--- word_filteration.py
+++ word_filteration.py
@@ -2,12 +2,14 @@
 import spacy
 
 nlp = spacy.load('en_core_web_sm')
+
 # Import the Matcher library
 from spacy.matcher import Matcher
 
 matcher = Matcher(nlp.vocab)
 
 import fact_detection
+import Model.rule_based_word_patterns as pattern_dict
 
 
 class word_filteration:
@@ -19,7 +21,7 @@ class word_filteration:
     def remove_stopwords(self, sentence_list):
 
         # declare list of words that need to be filtered out directly-stop words
-        words_filteration = ['hello', 'welcome', 'so', 'however', 'greetings', 'hi,', 'dude,',
+        words_filteration = ['welcome', 'so', 'greetings', 'hi', 'dude,',
                              'good morning', 'please', 'solar power']
 
         sent_list = []
@@ -38,5 +40,37 @@ class word_filteration:
                 filtered_text = " ".join(clean_sent)
                 # print(filtered_text)
                 sent_list.append(filtered_text)
+
+        self.remove_words_by_ruleBasedMatching(sent_list)
+
+    # added the word patterns of same word that need to be filtered out
+    def remove_words_by_ruleBasedMatching(self, sent_list):
+
+        for pattern in pattern_dict.dict:
+            matcher.add(pattern, None,  pattern_dict.dict[pattern])
+
+        for i in range(len(sent_list)):
+            doc = nlp(sent_list[i])
+            found_matches = matcher(doc)
+
+            sentence = ""
+            previous_end = None
+            if len(found_matches) != 0:
+                for matches in found_matches:
+                    #     print(matches)
+                    if previous_end == None:
+                        sentence = sentence + " " + str(doc[:matches[1]]).strip()
+                        previous_end = matches[2]
+
+                    else:
+
+                        sentence = sentence + " " + str(doc[previous_end:matches[1]]).strip()
+                        previous_end = matches[2]
+
+                sentence = sentence + " " + str(doc[previous_end:]).strip()
+                sent_list[i] = sentence.strip()
+
+        # for sent in sent_list:
+        #     print(sent)
 
         self.fact_detection_obj.detect_by_phrase_matching(sent_list)
